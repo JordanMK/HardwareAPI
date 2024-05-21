d64273f7b816a6dda50d5e39e78a0d75229a6a1b
jordanmalekera <115084133+JordanMalekeraVives@users.noreply.github.com>
2024-05-21 20:00:28 +0200

    Added component interfaces

---
+++ API/src/types/models/index.ts
@@ -0,0 +1,71 @@
+export enum ComponentType {
+	CPU = 'CPU',
+	GPU = 'GPU',
+	RAM = 'RAM',
+}
+
+export interface Component {
+	brand: string;
+	name: string;
+	componentType: ComponentType;
+}
+
+export interface CPUComponent extends Component {
+	family: string;
+	series: string;
+	generation: string;
+	architecture: string;
+	cores: number;
+	threads: number;
+	baseClock: number; // in MHz
+	boostClock: number; // in MHz
+	tdp: number; // in Watt
+	socket: string;
+	technologie: number; // in nm
+	integratedGraphics?: {
+		name: string;
+		brand: string;
+		generation: string;
+		architecture: string;
+		baseClock: number; // in MHz
+		boostClock: number; // in MHz
+	};
+	cache?: {
+		l1: string; // in kb
+		l2: string; // in kb
+		l3: string; // in kb
+	};
+	hyperthreading: boolean;
+	pcieSupport: string;
+	maxPcieLanes: number;
+	virtualisationSupport: boolean;
+}
+
+export interface GPUComponent extends Component {
+	family: string;
+	series: string;
+	generation: string;
+	architecture: string;
+	baseClock: number; // in MHz
+	boostClock: number; // in MHz
+	vram: number; // in MB
+	memoryType: string;
+	memorySpeed: number; // Memory speed in Gbps
+	tdp: number;
+	busWidth: number; // in bits
+	pcieSupport: string;
+	maxPcieLanes: number;
+	computeCores: number;
+	virtualisationSupport: boolean;
+}
+
+export interface RAMComponent extends Component {
+	series: string;
+	memoryType: string;
+	formFactor: 'DIMM' | 'SO-DIMM' | 'MicroDIMM' | 'Mini-DIMM';
+	eccSupport: boolean;
+	baseClock: number; // in Mhz
+	memorySpeed: number; // in MT/s
+	casLatency: string;
+	voltage: number; // in volts
+}
