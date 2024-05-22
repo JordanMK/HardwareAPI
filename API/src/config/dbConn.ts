1aefe871237725ca5b730fab7422d486947822e4
jordanmalekera <115084133+JordanMalekeraVives@users.noreply.github.com>
2024-05-22 08:54:25 +0200

    Changed config

--- API/src/config/dbConn.ts
+++ API/src/config/dbConn.ts
@@ -2,10 +2,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
 	try {
-		await mongoose.connect(
-			process.env.DATABASE_URI ?? 'mongodb://localhost/myDatabase'
-		);
-		console.log('MongoDB connected...');
+		await mongoose
+			.connect(process.env.DATABASE_URI as string)
+			.then(() => console.log('MongoDB connected...'))
+			.catch((error) => console.error(error));
 	} catch (err) {
 		console.error(err);
 		process.exit(1);
