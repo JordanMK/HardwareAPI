d5f0150a38d901ffa63fe359a8582485419a1b6f
jordanmalekera <115084133+JordanMalekeraVives@users.noreply.github.com>
2024-05-22 19:16:02 +0200

    Added logger

--- API/src/server.ts
+++ API/src/server.ts
@@ -6,16 +6,33 @@ import { serve, setup } from 'swagger-ui-express';
 import swaggerJson from './swagger/swagger.json';
 import connectDB from './config/dbConn';
 import { components } from './routes/api/v1/components';
-import { RAMComponents } from './routes/api/v1/components/ram';
+import path from 'path';
+import morgan from 'morgan';
+import fs from 'fs';
 
 dotenv.config();
-
 connectDB();
 
 const app: Express = express();
 const PORT: string | number = process.env.PORT || 3000;
 
+// Form data
+app.use(express.urlencoded({ extended: false }));
+
+// Json data
+app.use(express.json());
 app.use(bodyParser.json());
+
+// Static files
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Logging
+app.use(
+	morgan(':date[web] | :url | :method | :status | :remote-addr | :referrer', {
+		stream: fs.createWriteStream('./src/logs/access.log', { flags: 'a' }),
+	})
+);
+
 app.use('/api/v1/components', components);
 
 app.use('/api/v1/docs', serve, setup(swaggerJson));
