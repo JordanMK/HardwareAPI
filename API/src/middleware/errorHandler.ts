db0f19a5bf55b64ed08501c7081550516dfd7719
JordanMK <115084133+JordanMK@users.noreply.github.com>
2024-05-26 17:54:57 +0200

    Changed errorHandler

--- API/src/middleware/errorHandler.ts
+++ API/src/middleware/errorHandler.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
-import morgan from 'morgan';
-import logErrors from './logErrors';
+import formatter from 'date-fns';
+import { v4 as uuidv4 } from 'uuid';
+import fs from 'fs';
+import fsPromises from 'fs/promises';
+import path from 'path';
 
 const errorHandler = (
 	err: Error,
@@ -11,6 +14,24 @@ const errorHandler = (
 	logErrors(`${err.name}: ${err.message}`, 'errors.log');
 	console.error(err.stack);
 	res.status(500).json({ message: err.message });
+	next();
+};
+
+const logErrors = async (message: string, logName: string) => {
+	const dateTime = `${formatter.format(new Date(), 'dd/MM/yyy\tHH:mm:ss')}`;
+	const logItem = `${dateTime}\t${uuidv4()}\t${message}\n`;
+	try {
+		if (!fs.existsSync(path.join(__dirname, '../logs'))) {
+			await fsPromises.mkdir(path.join(__dirname, '../logs'));
+		}
+
+		await fsPromises.appendFile(
+			path.join(__dirname, '../logs', logName),
+			logItem
+		);
+	} catch (err) {
+		console.error(err);
+	}
 };
 
 export default errorHandler;
