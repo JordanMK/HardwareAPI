3943fa2d7d32b616bd06d9d465b8edf7e442d6ab
JordanMK <115084133+JordanMK@users.noreply.github.com>
2024-06-08 00:15:14 +0200

    Renamed controller

--- src/routes/api/v1/devices/computers.ts
+++ src/routes/api/v1/devices/computers.ts
@@ -3,32 +3,32 @@ import auth from '../../../../middleware/auth';
 import verifyRole from '../../../../middleware/verifyRole';
 import { UserRole } from '../../../../types/models';
 import validator from '../../../../middleware/validator';
-import computerController from '../../../../controllers/devices/computerController';
+import computersController from '../../../../controllers/devices/computersController';
 
 const router = express.Router();
 
 router
 	.route('/:id')
-	.get(computerController.getComputerById)
+	.get(computersController.getComputerById)
 	.put(
 		auth,
 		verifyRole(UserRole.ADMIN, UserRole.USER),
 		validator('computerUpdateSchema', 'body'),
-		computerController.updateComputer
+		computersController.updateComputer
 	)
-	.delete(auth, verifyRole(UserRole.ADMIN), computerController.deleteComputer);
+	.delete(auth, verifyRole(UserRole.ADMIN), computersController.deleteComputer);
 
 router
 	.route('/')
 	.get(
 		validator('computerQuerySchema', 'query'),
-		computerController.getComputers
+		computersController.getComputers
 	)
 	.post(
 		auth,
 		verifyRole(UserRole.ADMIN, UserRole.USER),
 		validator('computerCreateSchema', 'body'),
-		computerController.createComputer
+		computersController.createComputer
 	);
 
 export const computers = router;
