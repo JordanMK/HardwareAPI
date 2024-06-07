30e1b528a3a1c49b8583f48f03aa0ab7489a95f1
JordanMK <115084133+JordanMK@users.noreply.github.com>
2024-06-07 22:54:12 +0200

    Fixed optional schema

--- src/validators/makeOptional.ts
+++ src/validators/makeOptional.ts
@@ -1,8 +1,14 @@
 import Joi from 'joi';
 
 const makeOptional = (schema: Joi.ObjectSchema) => {
-	return schema.fork(Object.keys(schema.describe().keys), (schema) =>
-		schema.optional()
+	return schema.fork(Object.keys(schema.describe().keys), (subSchema) =>
+		subSchema.type == 'object'
+			? subSchema
+					.optional()
+					.fork(Object.keys(subSchema.describe().keys), (childSchema) =>
+						childSchema.optional()
+					)
+			: subSchema.optional()
 	);
 };
 
